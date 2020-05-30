5fc7bdfef488c4ea80a6785debf9d5e4e99cb434
monmima <monmima@protonmail.com>
2020-05-29 20:22:46 -0400

    barebone server with ejs loading HTML

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+/**
+ * path is a core module of node
+ */
+const path = require("path");
+
+var app = express();
+
+app.use(bodyParser());
+app.use(cors());
+
+/**
+ * concatenate the current working directory
+ * and a folder called views
+ */
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", function(request, response) {
+    response.render("index");
+});
+
+app.listen(3000, function() {
+    console.log("Running on port 3000!");
+});
+
